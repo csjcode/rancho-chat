5e6f58590185007547f184976a6f252bd6f70d36
Chris St. John <csjcode@gmail.com>
2022-11-25 21:55:13 -0600

    Basics/fonts/splash installed

--- App.tsx
+++ App.tsx
@@ -1,45 +1,55 @@
-import { StatusBar } from "expo-status-bar";
-import { useState, useEffect, useCallback } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import Total from "./Total";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [numSandwiches, setNumSandwiches] = useState(0);
-  const [numWater, setNumWater] = useState(0);
-  const [numMayo, setNumMayo] = useState(0);
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
 
-  const sandwichPrice = 5;
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          black: require("./assets/fonts//Roboto-Black.ttf"),
+          blackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          bold: require("./assets/fonts/Roboto-Bold.ttf"),
+          boldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          italic: require("./assets/fonts/Roboto-Italic.ttf"),
+          light: require("./assets/fonts/Roboto-Light.ttf"),
+          lightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+          medium: require("./assets/fonts/Roboto-Medium.ttf"),
+          mediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          regular: require("./assets/fonts/Roboto-Regular.ttf"),
+          thin: require("./assets/fonts/Roboto-Thin.ttf"),
+          thinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        });
+      } catch (error: any) {
+        console.log(error);
+      } finally {
+        setAppIsLoaded(true);
+      }
+    };
 
-  const addSandwich = () => {
-    setNumSandwiches((numSandwiches) => numSandwiches + 1);
-  };
+    prepare();
+  }, []);
 
-  const addWater = () => {
-    setNumWater((numWater) => numWater + 1);
-  };
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
 
-  const addMayo = () => {
-    setNumMayo((numMayo) => numMayo + 1);
-  };
-
-  const getTotal = useCallback(() => {
-    return numSandwiches * sandwichPrice;
-  }, [numSandwiches]);
+  if (!appIsLoaded) {
+    return null;
+  }
 
   return (
-    <SafeAreaProvider style={styles.container}>
+    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
       <SafeAreaView>
-        <Text style={styles.label}>{"Sandwiches: " + numSandwiches}</Text>
-        <Button title="Add sandwich" onPress={addSandwich} />
-
-        <Text style={styles.label}>{"Water: " + numWater}</Text>
-        <Button title="Add water" onPress={addWater} />
-
-        <Text style={styles.label}>{"Mayo: " + numMayo}</Text>
-        <Button title="Add Mayo" onPress={addMayo}></Button>
-
-        <Total getTotal={getTotal} />
+        <Text style={styles.label}>Hi everyone!</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -55,5 +65,6 @@ const styles = StyleSheet.create({
   label: {
     color: "black",
     fontSize: 18,
+    fontFamily: "regular",
   },
 });
