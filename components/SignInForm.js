e8602c3f83f4b8007f7dd8f702da4d73b673056d
Chris St. John <csjcode@gmail.com>
2022-11-28 17:11:51 -0600

    Settings update DB

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -72,7 +72,7 @@ const SignInForm = (props) => {
         autoCapitalize="none"
         keyboardType="email-address"
         onInputChanged={inputChangedHandler}
-        value={formState.inputValues.email}
+        initialValue={formState.inputValues.email}
         errorText={formState.inputValidities['email']}
       />
 
@@ -84,7 +84,7 @@ const SignInForm = (props) => {
         autoCapitalize="none"
         secureTextEntry
         onInputChanged={inputChangedHandler}
-        value={formState.inputValues.password}
+        initialValue={formState.inputValues.password}
         errorText={formState.inputValidities['password']}
       />
 
