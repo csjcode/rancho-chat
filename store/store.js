187b4dcefab39ec3820e2c786123107c92b906d8
Chris St. John <csjcode@gmail.com>
2022-11-29 17:31:04 -0600

    Save the chats list

--- store/store.js
+++ store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from './authSlice'
+import chatSlice from './chatSlice'
 import userSlice from './userSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     users: userSlice,
+    chats: chatSlice,
   },
 })
