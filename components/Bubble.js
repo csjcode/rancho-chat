ab5b1f2b5b7e8b9174f6a49e76b70b821150e02d
Chris St. John <csjcode@gmail.com>
2022-11-30 11:09:43 -0600

    Styling their messages

--- components/Bubble.js
+++ components/Bubble.js
@@ -1,53 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import colors from '../constants/colors';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import colors from '../constants/colors'
 
-const Bubble = props => {
-    const { text, type } = props;
+const Bubble = (props) => {
+  const { text, type } = props
 
-    const bubbleStyle = { ...styles.container };
-    const textStyle = { ...styles.text };
+  const bubbleStyle = { ...styles.container }
+  const textStyle = { ...styles.text }
+  const wrapperStyle = { ...styles.wrapperStyle }
 
-    switch (type) {
-        case "system":
-            textStyle.color = '#65644A';
-            bubbleStyle.backgroundColor = colors.beige;
-            bubbleStyle.alignItems = 'center';
-            bubbleStyle.marginTop = 10;
-            break;
-    
-        default:
-            break;
-    }
+  switch (type) {
+    case 'system':
+      textStyle.color = '#65644A'
+      bubbleStyle.backgroundColor = colors.beige
+      bubbleStyle.alignItems = 'center'
+      bubbleStyle.marginTop = 10
+      break
+    case 'error':
+      bubbleStyle.backgroundColor = colors.red
+      textStyle.color = 'white'
+      bubbleStyle.marginTop = 10
+      break
+    case 'myMessage':
+      wrapperStyle.justifyContent = 'flex-end'
+      bubbleStyle.backgroundColor = '#E7FED6'
+      bubbleStyle.maxWidth = '90%'
+      break
+    case 'theirMessage':
+      wrapperStyle.justifyContent = 'flex-start'
+      bubbleStyle.maxWidth = '90%'
+      break
 
-    return (
-        <View style={styles.wrapperStyle}>
-            <View style={bubbleStyle}>
-                <Text style={textStyle}>
-                    {text}
-                </Text>
-            </View>
-        </View>
-    )
+    default:
+      break
+  }
+
+  return (
+    <View style={wrapperStyle}>
+      <View style={bubbleStyle}>
+        <Text style={textStyle}>{text}</Text>
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    wrapperStyle: {
-        flexDirection: 'row',
-        justifyContent: 'center'
-    },
-    container: {
-        backgroundColor: 'white',
-        borderRadius: 6,
-        padding: 5,
-        marginBottom: 10,
-        borderColor: '#E2DACC',
-        borderWidth: 1
-    },
-    text: {
-        fontFamily: 'regular',
-        letterSpacing: 0.3
-    }
+  wrapperStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 6,
+    padding: 5,
+    marginBottom: 10,
+    borderColor: '#E2DACC',
+    borderWidth: 1,
+  },
+  text: {
+    fontFamily: 'regular',
+    letterSpacing: 0.3,
+  },
 })
 
-export default Bubble;
+export default Bubble
