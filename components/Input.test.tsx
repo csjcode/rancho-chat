bbd14cfa6bf20f7ce952e603b83bae2b93f11961
Chris St. John <csjcode@gmail.com>
2022-12-30 16:09:08 -0600

    Update input tests

--- components/Input.test.tsx
+++ components/Input.test.tsx
@@ -2,21 +2,23 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Input from "./Input";
 
-test("Input component renders correctly", () => {
-  const tree = renderer
-    .create(
-      <Input
-        initialValue="Test value"
-        label="Test label"
-        icon="testIcon"
-        iconPack="testIconPack"
-        iconSize={15}
-        addStyles={{ testStyle: "testValue" }}
-        onInputChanged={() => {}}
-        errorText={["Test error"]}
-        id={1}
-      />
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe("Input component", () => {
+  test("Input component renders correctly", () => {
+    const tree = renderer
+      .create(
+        <Input
+          initialValue="Test value"
+          label="Test label"
+          icon="testIcon"
+          iconPack="testIconPack"
+          iconSize={15}
+          addStyles={{ testStyle: "testValue" }}
+          onInputChanged={() => {}}
+          errorText={["Test error"]}
+          id={1}
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
