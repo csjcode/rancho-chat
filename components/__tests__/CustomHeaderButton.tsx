c59348ee1fd39d2902c2091d105a60137d5b114c
Chris St. John <csjcode@gmail.com>
2022-12-30 12:10:29 -0600

    Initial tests

---
+++ components/__tests__/CustomHeaderButton.tsx
@@ -0,0 +1,29 @@
+// Unit Tests
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import CustomHeaderButton from "../CustomHeaderButton";
+import colors from "../../constants/colors/colors";
+
+describe("CustomHeaderButton component", () => {
+  xit("Renders CustomHeaderButton component properly", () => {
+    const { getByTestId } = render(<CustomHeaderButton />);
+    const component = getByTestId("CustomHeaderButton");
+    expect(component).toBeTruthy();
+  });
+  // it("Matches the snapshot", () => {
+  //   // const { asJSON } = render(<CustomHeaderButton />);
+  //   // expect(asJSON()).toMatchSnapshot();
+  // });
+  // it('Renders an <Ionicons /> component', () => {
+  //   const { getByTestId } = render(<CustomHeaderButton />);
+  //   const component = getByTestId('Ionicons');
+  //   expect(component).toBeTruthy();
+  // });
+  // it('Passes in the correct props to the <Ionicons /> component', () => {
+  //   const { getByTestId } = render(<CustomHeaderButton color={colors.blue} />);
+  //   const component = getByTestId('Ionicons');
+  //   expect(component.props.iconSize).toBe(23);
+  //   expect(component.props.color).toBe(colors.blue);
+  // });
+});
+// The tests above check to make sure that the CustomHeaderButton component renders properly, that it matches the snapshot, that it renders an Ionicons component, and that it passes in the correct props to the Ionicons component.
