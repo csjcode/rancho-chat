2981ee534a360b41fae702cc0c5ea6b0c3ff678f
Chris St. John <csjcode@gmail.com>
2022-12-31 08:08:03 -0600

    Fix test template

--- components/__tests__/testTemplate.test.tsx
+++ components/__tests__/testTemplate.test.tsx
@@ -1,19 +1,24 @@
 import renderer from "react-test-renderer";
 import ReplyTo from "../ReplyTo";
 import TokenPrice from "../TokenPrice";
-
-describe("Test suite", () => {
-  xit("renders correctly", () => {
-    // const mockProps = {
-    //   text: "This is a test message",
-    //   user: {
-    //     firstName: "John",
-    //     lastName: "Doe",
-    //   },
-    //   onCancel: jest.fn(),
-    // };
-    //  const tree = ReactTestRenderer.create(<ReplyTo {...mockProps} />).toJSON();
-    const tree = renderer.create(<TokenPrice />).toJSON();
-    expect(tree).toMatchSnapshot();
+describe("Template Test suite", () => {
+  it("Test template", () => {
+    expect(1 + 1).toEqual(2);
   });
 });
+
+// describe("Test suite", () => {
+//   xit("renders correctly", () => {
+//     // const mockProps = {
+//     //   text: "This is a test message",
+//     //   user: {
+//     //     firstName: "John",
+//     //     lastName: "Doe",
+//     //   },
+//     //   onCancel: jest.fn(),
+//     // };
+//     //  const tree = ReactTestRenderer.create(<ReplyTo {...mockProps} />).toJSON();
+//     const tree = renderer.create(<TokenPrice />).toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
